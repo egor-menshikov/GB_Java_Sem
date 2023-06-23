214837291eae721711e408529256f115093a50c0
Egor Menshikov <r3born.fs@gmail.com>
2023-06-23 14:32:39 +0300

    s1t3 done

--- src/Sem_1/Task_3/Main.java
+++ src/Sem_1/Task_3/Main.java
@@ -1,4 +1,19 @@
 package Sem_1.Task_3;
 
+//Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.
+
 public class Main {
+    public static void main(String[] args) {
+        String text = "Добро пожаловать на курс по Java";
+        System.out.println(rev(text));
+    }
+
+    static String rev(String line) {
+        StringBuilder result = new StringBuilder();
+        String[] text = line.split(" ");
+        for (int i = text.length - 1; i >= 0; i--) {
+            result.append(text[i]).append(" ");
+        }
+        return result.toString();
+    }
 }
