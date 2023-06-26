c7a00321643b0955d7a75a11f4540d3a68a24019
Egor Menshikov <r3born.fs@gmail.com>
2023-06-26 13:27:13 +0300

    HW1 Task_4rework in progress

--- src/HOMEWORKS/HW1/Task_4/Main.java
+++ src/HOMEWORKS/HW1/Task_4/Main.java
@@ -3,13 +3,7 @@ package HOMEWORKS.HW1.Task_4;
 
 public class Main {
     public static void main(String[] args) {
-//        String equation = "2? + 1?5 = 168";
-//        String equation = "14?9 + 1?64 = 2833";
-//        String A = "14?9";
-//        String B = "1?64";
-//        StringBuilder C = new StringBuilder("2833");
-//        C.setCharAt(1, '+');
-//        System.out.println(C);
+        StringBuilder equation = new StringBuilder("14?9 + 1?64 = 2833");
     }
 
     public static StringBuilder eq_calc(StringBuilder a, StringBuilder b, StringBuilder c, int pos, int digit) {
@@ -17,9 +11,11 @@ public class Main {
 
         if (a.length() < pos && b.length() < pos && sum == Integer.parseInt(String.valueOf(c))) {
             return new StringBuilder(a + " + " + b + " = " + c);
-        } else if (a.length() < pos && b.length() < pos && sum != Integer.parseInt(String.valueOf(c))) {
+        }
+        else if (a.length() < pos && b.length() < pos && sum != Integer.parseInt(String.valueOf(c))) {
             return new StringBuilder("Уравнение невозможно решить");
-        } else {
+        }
+        else {
             if (a.length() >= pos && b.length() >= pos) {
                 char char_a = a.charAt(a.length() - pos);
                 char char_b = b.charAt(b.length() - pos);
@@ -58,6 +54,9 @@ public class Main {
                         a.setCharAt(a.length() - pos, char_a);
                         return eq_calc(a, b, c, pos + 1, 1);
                     }
+                }
+                else {
+                    
                 }
             }
         }
