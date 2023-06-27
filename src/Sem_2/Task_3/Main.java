b6346a72bcc685313bc743fc731fc958d36a2f56
Egor Menshikov <r3born.fs@gmail.com>
2023-06-27 21:12:44 +0300

    Sem_2 done

--- src/Sem_2/Task_3/Main.java
+++ src/Sem_2/Task_3/Main.java
@@ -1,9 +1,26 @@
 package Sem_2.Task_3;
 
 
+
 //Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
 //
-//Полиндром - Слово или фраза, которые одинаково читаются слева направо и справа налево.
+//Палиндром - Слово или фраза, которые одинаково читаются слева направо и справа налево.
 public class Main {
+    public static void main(String[] args) {
+        String sentence = "abcfaaaafcba";
 
+        System.out.println(pali(sentence));
+    }
+
+    public static boolean pali(String str) {
+        boolean flag = true;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
 }
--- src/Sem_2/Task_5/Main.java
+++
@@ -1,4 +0,0 @@
-package Sem_2.Task_5;
-
-public class Main {
-}
