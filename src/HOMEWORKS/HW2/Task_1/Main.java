250edaca4a3e5d4c13f7a1b4fa44f39080628bb5
Egor Menshikov <r3born.fs@gmail.com>
2023-07-01 16:09:11 +0300

    hw2 task_1 almost done

--- src/HOMEWORKS/HW2/Task_1/Main.java
+++ src/HOMEWORKS/HW2/Task_1/Main.java
@@ -1,48 +1,59 @@
 package HOMEWORKS.HW2.Task_1;
 
-import java.io.File;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 
 public class Main {
+    public static String pathPr = System.getProperty("user.dir");
+    public static String pathFile = pathPr + "\\src\\HOMEWORKS\\HW2\\Task_1\\string.json";
+
     public static void main(String[] args) {
-//        String json = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
-//                "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +
-//                "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
-        String json = reader();
-        System.out.println(json);
-        ArrayList<StringBuilder> list = new ArrayList<>();
-        StringBuilder group = new StringBuilder();
-        boolean add_flag = false;
-
-        for (int i = 0; i < json.length(); i++) {
-            if (add_flag && json.charAt(i) != '}') group.append(json.charAt(i));
-            if (json.charAt(i) == '{') add_flag = true;
-            if (json.charAt(i) == '}') {
-                list.add(group);
-                add_flag = false;
-                group = new StringBuilder();
-            }
-        }
-        for (StringBuilder item : list
-             ) {
-            System.out.println(item);
-        }
-
-
+        String json = reader(pathFile);
+        String[][][] textKeyVal = parsed(json);
+        System.out.println(parsedOutput(textKeyVal));
     }
 
-    public static String reader() {
+    public static String parsedOutput(String[][][] input) {
+        StringBuilder result = new StringBuilder();
+        for (String[][] strings : input) {
+            result.append("Студент ").append(strings[0][1])
+                    .append(" получил ").append(strings[1][1])
+                    .append(" по предмету ").append(strings[2][1]).append("\n");
+        }
+        return result.toString();
+    }
+
+
+    public static String[][][] parsed(String input) {
+        String text = input.replaceAll("[^,}:а-яА-Я0-9]", "");
+
+        String[] textLines = text.split("},");
+        for (int i = 0; i < textLines.length; i++) {
+            textLines[i] = textLines[i].replace("}", "");
+        }
+
+        String[][][] result = new String[textLines.length][3][2];
+        for (int i = 0; i < textLines.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 2; k++) {
+                    String[] pairs = textLines[i].split(",");
+                    String[] values = pairs[j].split(":");
+                    result[i][j][k] = values[k];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String reader(String path) {
         StringBuilder result = new StringBuilder();
         int c;
         char ch;
-        String pathPr = System.getProperty("user.dir");
-        String pathFile = pathPr + "\\src\\HOMEWORKS\\HW2\\Task_1\\string.json";
         try {
-            FileReader reader = new FileReader(pathFile, StandardCharsets.UTF_8);
+            FileReader reader = new FileReader(path, StandardCharsets.UTF_8);
             while ((c = reader.read()) != -1) {
                 ch = (char) c;
                 result.append(ch);
@@ -53,4 +64,6 @@ public class Main {
         }
         return result.toString();
     }
+
+//    public static void writer()
 }
