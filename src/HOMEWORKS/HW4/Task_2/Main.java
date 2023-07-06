544280b450dbd7c675df9e70fcd2484c931b8a76
Egor Menshikov <r3born.fs@gmail.com>
2023-07-06 11:22:25 +0300

    HW4 Task_2 in progress

--- src/HOMEWORKS/HW4/Task_2/Main.java
+++ src/HOMEWORKS/HW4/Task_2/Main.java
@@ -1,7 +1,14 @@
 package HOMEWORKS.HW4.Task_2;
 
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//        enqueue() - помещает элемент в конец очереди,
+//        dequeue() - возвращает первый элемент из очереди и удаляет его,
+//        first() - возвращает первый элемент из очереди, не удаляя.
 public class Main {
     public static void main(String[] args) {
-
+        MyQueue mq = new MyQueue();
+        int a = 13;
+        mq.enqueue(a);
+        mq.enqueue("14");
     }
 }
--- src/HOMEWORKS/HW4/Task_2/MyQueue.java
+++ src/HOMEWORKS/HW4/Task_2/MyQueue.java
@@ -1,7 +1,24 @@
 package HOMEWORKS.HW4.Task_2;
 
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//        enqueue() - помещает элемент в конец очереди,
+//        dequeue() - возвращает первый элемент из очереди и удаляет его,
+//        first() - возвращает первый элемент из очереди, не удаляя.
+
 import java.util.LinkedList;
 
 public class MyQueue {
     LinkedList<Object> myQ = new LinkedList<>();
+
+    int size() {
+        return myQ.size();
+    }
+
+    void enqueue(Object item) {
+        myQ.add(item);
+    }
+
+    Object dequeue() {
+        return myQ.pollFirst();
+    }
 }
