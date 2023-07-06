9105cea3bbf5b47c46dd70f066b2c5353450327a
Egor Menshikov <r3born.fs@gmail.com>
2023-07-06 10:59:36 +0300

    HW4 Task_1 done

--- src/Lectures/Lect_4/Main.java
+++ src/Lectures/Lect_4/Main.java
@@ -1,4 +1,4 @@
-package Lect_4;
+package Lectures.Lect_4;
 
 public class Main {
     public static void main(String[] args) {
--- src/Lectures/Lect_4/Que_ue.java
+++ src/Lectures/Lect_4/Que_ue.java
@@ -1,7 +1,6 @@
-package Lect_4;
+package Lectures.Lect_4;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
