49dcc6f55ddb9acc1038d954594def787b8926a5
Egor Menshikov <r3born.fs@gmail.com>
2023-07-13 00:32:04 +0300

    HW_Final started

---
+++ src/HOMEWORKS/HW6_final/Laptop.java
@@ -0,0 +1,13 @@
+package HOMEWORKS.HW6_final;
+
+public class Laptop {
+        String name;
+        String cpu;
+        int ram;
+        int ssd;
+        String gpu;
+        double screen;
+        Color color;
+
+        private enum Color {SILVER, BLACK}
+}
