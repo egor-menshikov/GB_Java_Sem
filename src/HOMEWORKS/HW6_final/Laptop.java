57b8da45c85ad65bcdae5136f94593ff1e3eb461
Egor Menshikov <r3born.fs@gmail.com>
2023-07-14 12:30:22 +0300

    HW_Final in progress

--- src/HOMEWORKS/HW6_final/Laptop.java
+++ src/HOMEWORKS/HW6_final/Laptop.java
@@ -1,10 +1,27 @@
 package HOMEWORKS.HW6_final;
 
 public class Laptop {
-        String name, cpu, gpu;
-        int ram, ssd;
-        double screen;
-        Color color;
+    String name, cpu, gpu;
+    int ram, ssd, price;
+    double screen;
+    Color color;
 
-        public enum Color {SILVER, BLACK}
+    public enum Color {SILVER, BLACK, RED, BLUE}
+
+    public Laptop(String name, String cpu, String gpu, int ram, int ssd, int price, double screen, Color color) {
+        this.name = name;
+        this.cpu = cpu;
+        this.gpu = gpu;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.price = price;
+        this.screen = screen;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\nCPU: " + cpu + " | GPU: " + gpu + "\n" +
+                ram + " GB | " + ssd + " GB ssd | " + screen + "\" | " + color + " | " + price + "руб." + "\n";
+    }
 }
--- src/HOMEWORKS/HW6_final/Main.java
+++ src/HOMEWORKS/HW6_final/Main.java
@@ -1,7 +1,19 @@
 package HOMEWORKS.HW6_final;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-
+        Laptop l1 = new Laptop("Acer Aspire 3", "Ryzen 3 7320U", "AMD Radeon 610M",
+                8, 256, 42570, 15.6, Laptop.Color.SILVER);
+        Laptop l2 = new Laptop("Acer Aspire 3", "Ryzen 3 7320U", "AMD Radeon 610M",
+                8, 257, 42570, 15.6, Laptop.Color.SILVER);
+        HashSet<Laptop> pricelist = new HashSet<>();
+        pricelist.add(l1);
+        pricelist.add(l2);
+        for (Laptop item : pricelist) {
+            System.out.println(item);
+        }
     }
 }
